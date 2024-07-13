7b8dd42f83c764700620c4b3fd4bc0fa6b2195bd
Peter Omollo <omollpeter@gmail.com>
2024-07-13 09:59:07 +0300

    Configure a new route for the API

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,7 +7,7 @@ This module contains Blueprint for the GlitchSpy API
 from flask import Blueprint
 
 
-app_views = Blueprint("app_views", __name__, url_prefix="/gspy/api/v1")
+app_views = Blueprint("app_views", __name__, url_prefix="/api/gspy/v1")
 
 
 from api.v1.views.index import *
